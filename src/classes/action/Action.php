021d9706a712638830552b8d6b946941f8fc32ac
Александр Зубрицкий <5998971@gmail.com>
2020-04-22 09:22:03 +0300

    удаляет ненужные константы из прошлого задания

--- src/classes/action/Action.php
+++ src/classes/action/Action.php
@@ -6,24 +6,7 @@ abstract class Action
 {
 public $actionName;
 public $innerName;
-    // statuses
 
-    const STATUS_NEW = 'new'; // новое
-    const STATUS_PROGRESS = 'progress'; // в работе
-    const STATUS_CANCEL = 'cancelled'; // отменено
-    const STATUS_COMPLETE = 'completed'; // выполнено
-    const STATUS_FAIL = 'failed'; // провалено
-
-
-
-    // actions
-    const ACTION_NEW = 'new task';
-    const ACTION_CANCEL = 'cancel'; // отменить - заказчик
-    const ACTION_RESPONSE = 'response'; // откликнуться - исполнитель
-    const ACTION_COMPLETE = 'complete'; // завершить - заказчик
-    const ACTION_REFUSE = 'refuse'; // отказаться - исполнитель
-
-    public $clientId, $executorId, $currentUserId;
 
     abstract public function checkAccess($executorId, $clientId, $currentUserId);
 
