da436fafa043a0b34e22df6aaa0cad963a41b605
Александр Зубрицкий <5998971@gmail.com>
2020-04-24 10:53:33 +0300

    исключения в статусах

--- src/classes/Task.php
+++ src/classes/Task.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+
 namespace taskForce\classes;
 
 
@@ -14,27 +15,30 @@ use taskForce\exceptions\TaskException;
 class Task
 {
 
-const STATUS_NEW = 'new';
+    const STATUS_NEW = 'new';
     // statuses
-const STATUS_PROGRESS = 'progress'; // Новое	Задание опубликовано, исполнитель ещё не найден
+    const STATUS_PROGRESS = 'progress'; // Новое	Задание опубликовано, исполнитель ещё не найден
     const STATUS_CANCEL = 'cancelled'; // В работе	Заказчик выбрал исполнителя для задания
     const STATUS_COMPLETE = 'completed'; // Отменено	Заказчик отменил задание
     const STATUS_FAIL = 'failed'; // Выполнено	Заказчик отметил задание как выполненное
-        const ACTION_NEW = 'new task'; // Провалено	Исполнитель отказался от выполнения задания
+    const ACTION_NEW = 'new task'; // Провалено	Исполнитель отказался от выполнения задания
 
 
     // actions
-const ACTION_CANCEL = 'cancel';
+    const ACTION_CANCEL = 'cancel';
     const ACTION_RESPONSE = 'response'; // отменить - заказчик
     const ACTION_COMPLETE = 'complete'; // откликнуться - исполнитель
-const ACTION_REFUSE = 'refuse'; // завершить - заказчик
-        public static $mapAction = [
+    const ACTION_REFUSE = 'refuse'; // завершить - заказчик
+
+    public static $mapAction = [
         self::ACTION_CANCEL => 'Отменить',
         self::ACTION_RESPONSE => 'Откликнуться',
         self::ACTION_COMPLETE => 'Выполнено',
         self::ACTION_REFUSE => 'Отказаться'
-    ]; // отказаться - исполнитель
+    ];
+
     public $actionNew, $actionCancel, $actionComplete, $actionRefuse, $actionResponse;
+
     public $mapStatus = [ // вернуть статус на русском языке
         self::STATUS_NEW => 'Новое',
         self::STATUS_PROGRESS => 'В работе',
@@ -50,8 +54,8 @@ const ACTION_REFUSE = 'refuse'; // завершить - заказчик
 
     public function __construct(string $status, int $executorID, int $clientId, int $currentUserId) // конструктор
     {
-        if ($status === '') {
-            throw new TaskException('Пустое поле статуса');
+        if (!isset($this->mapStatus[$status])) {
+            throw new TaskException('Неверно передан статус');
         }
 
         $this->clientId = $clientId;
@@ -67,19 +71,12 @@ const ACTION_REFUSE = 'refuse'; // завершить - заказчик
 
     }
 
-    public function getAvailableActions():array
+    public function getAvailableActions(): array
     {
         $actions = []; // пустой массив действий
         $executor = $this->currentUserId !== $this->clientId; // исполнитель
         $client = $this->currentUserId == $this->clientId; // заказчик
 
-        if (!isset($executor)) {
-            throw new TaskException('исполнитель не найден');
-        }
-
-        if (!isset($client)) {
-            throw new TaskException('заказчик не найден');
-        }
 
         switch ($this->status) {
             case self::STATUS_NEW and $executor:
