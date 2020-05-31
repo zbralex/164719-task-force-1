fe5ab0dd4d2f300854a9717824849f2b4c3bc102
Александр Зубрицкий <5998971@gmail.com>
2020-05-31 14:27:58 +0300

    генерирует модели бд

---
+++ yii-taskforce/frontend/models/Attachment.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "attachment".
+ *
+ * @property int $id
+ * @property int $task_id
+ * @property string $url
+ *
+ * @property Task $task
+ */
+class Attachment extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'attachment';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['task_id', 'url'], 'required'],
+            [['task_id'], 'integer'],
+            [['url'], 'string', 'max' => 255],
+            [['task_id'], 'exist', 'skipOnError' => true, 'targetClass' => Task::className(), 'targetAttribute' => ['task_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'task_id' => 'Task ID',
+            'url' => 'Url',
+        ];
+    }
+
+    /**
+     * Gets query for [[Task]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Task::className(), ['id' => 'task_id']);
+    }
+}
---
+++ yii-taskforce/frontend/models/Response.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "response".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $price
+ * @property string $comment
+ * @property string|null $responsed_at
+ * @property int $task_id
+ * @property string $status
+ *
+ * @property User $user
+ * @property Task $task
+ */
+class Response extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'response';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'price', 'comment', 'task_id', 'status'], 'required'],
+            [['user_id', 'price', 'task_id'], 'integer'],
+            [['responsed_at'], 'safe'],
+            [['comment', 'status'], 'string', 'max' => 255],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+            [['task_id'], 'exist', 'skipOnError' => true, 'targetClass' => Task::className(), 'targetAttribute' => ['task_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'price' => 'Price',
+            'comment' => 'Comment',
+            'responsed_at' => 'Responsed At',
+            'task_id' => 'Task ID',
+            'status' => 'Status',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * Gets query for [[Task]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTask()
+    {
+        return $this->hasOne(Task::className(), ['id' => 'task_id']);
+    }
+}
