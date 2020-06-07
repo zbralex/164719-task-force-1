cb91b53fe502af803edb9bb4e3f3fe98838714c0
Александр Зубрицкий <5998971@gmail.com>
2020-06-07 10:30:45 +0300

    выводит пользователей

--- yii-taskforce/frontend/controllers/UsersController.php
+++ yii-taskforce/frontend/controllers/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace frontend\controllers;
 
+use frontend\models\UserInfo;
+
 use Yii;
 use yii\base\InvalidArgumentException;
 use yii\web\BadRequestHttpException;
@@ -13,7 +15,10 @@ class UsersController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+	    $users = UserInfo::findAll(['role_id'=> '1']);
+        return $this->render('index', [
+        	'users' => $users
+        ]);
     }
 
 }
--- yii-taskforce/frontend/views/users/index.php
+++ yii-taskforce/frontend/views/users/index.php
@@ -1,6 +1,13 @@
 <?php
 /* @var $this yii\web\View */
+
+$this->title = 'Users';
+$this->params['breadcrumbs'][] = $this->title;
+$users = $users;
+
+
 ?>
+
 <main class="page-main">
     <div class="main-container page-container">
         <section class="user__search">
@@ -19,6 +26,7 @@
                 </ul>
             </div>
             <div class="content-view__feedback-card user__search-wrapper">
+	            <?php foreach ($users as $user): ?>
                 <div class="feedback-card__top">
                     <div class="user__search-icon">
                         <a href="#"><img src="./img/man-glasses.jpg" width="65" height="65"></a>
@@ -26,7 +34,7 @@
                         <span>6 отзывов</span>
                     </div>
                     <div class="feedback-card__top--name user__search-card">
-                        <p class="link-name"><a href="#" class="link-regular">Астахов Павел</a></p>
+                        <p class="link-name"><a href="#" class="link-regular"><?= $user->name;?></a></p>
                         <span></span><span></span><span></span><span></span><span class="star-disabled"></span>
                         <b>4.25</b>
                         <p class="user__search-content">
@@ -37,6 +45,8 @@
                     </div>
                     <span class="new-task__time">Был на сайте 25 минут назад</span>
                 </div>
+	            <?php endforeach; ?>
+
                 <div class="link-specialization user__search-link--bottom">
                     <a href="#" class="link-regular">Ремонт</a>
                     <a href="#" class="link-regular">Курьер</a>
