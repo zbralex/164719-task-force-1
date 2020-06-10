cd2f7ddcc561b81a2a2eb9b7c7306a6bddb17eae
Александр Зубрицкий <5998971@gmail.com>
2020-06-10 14:38:57 +0300

    исправляет обращения к данным

--- frontend/views/tasks/index.php
+++ frontend/views/tasks/index.php
@@ -15,13 +15,13 @@
                 <div class="new-task__card">
                     <div class="new-task__title">
                         <a href="#" class="link-regular"><h2><?= $task->name?></h2></a>
-                        <a  class="new-task__type link-regular" href="#"><p><?= $task->category_id ?></p></a>
+                        <a  class="new-task__type link-regular" href="#"><p><?= $task->categories->name ?></p></a>
                     </div>
-                    <div class="new-task__icon new-task__icon--<?= $task->name ?>"></div>
+                    <div class="new-task__icon new-task__icon--<?= $task->categories->icon ?>"></div>
                     <p class="new-task_description">
-	                    <?= isset($task['description']) ? $task['description'] : "" ?>
+	                    <?= $task->description ?>
                     </p>
-                    <b class="new-task__price new-task__price--<?= isset($task['icon']) ? $task['icon'] : "" ?>"><?= isset($task['price']) ? $task['price'] : "" ?><b> ₽</b></b>
+                    <b class="new-task__price new-task__price--<?= $task->categories->icon ?>"><?= $task->price ?><b> ₽</b></b>
                     <p class="new-task__place">Санкт-Петербург, Центральный район</p>
                     <span class="new-task__time">4 часа назад</span>
                 </div>
