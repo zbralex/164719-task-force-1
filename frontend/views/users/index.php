25a57209879d691f594ee2ea3e2f9e1201285256
Александр Зубрицкий <5998971@gmail.com>
2020-07-13 22:18:18 +0300

    добавляет показ детальной страницы задания

--- frontend/views/users/index.php
+++ frontend/views/users/index.php
@@ -66,56 +66,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				<?php endforeach; ?>
 
 			</div>
-<!--			<div class="content-view__feedback-card user__search-wrapper">-->
-<!--				<div class="feedback-card__top">-->
-<!--					<div class="user__search-icon">-->
-<!--						<a href="#"><img src="./img/user-man2.jpg" width="65" height="65"></a>-->
-<!--						<span>6 заданий</span>-->
-<!--						<span>3 отзывов</span>-->
-<!--					</div>-->
-<!--					<div class="feedback-card__top--name user__search-card">-->
-<!--						<p class="link-name"><a href="#" class="link-regular">Миронов Алексей</a></p>-->
-<!--						<span></span><span></span><span></span><span></span><span class="star-disabled"></span>-->
-<!--						<b>4.25</b>-->
-<!--						<p class="user__search-content">-->
-<!--							Как принято считать, акционеры крупнейших компаний формируют глобальную-->
-<!--							экономическую сеть и при этом - рассмотрены исключительно в разрезе-->
-<!--							маркетинговых и финансовых предпосылок-->
-<!--						</p>-->
-<!--					</div>-->
-<!--					<span class="new-task__time">Был на сайте час назад</span>-->
-<!--				</div>-->
-<!--				<div class="link-specialization user__search-link--bottom">-->
-<!--					<a href="#" class="link-regular">Ремонт</a>-->
-<!--					<a href="#" class="link-regular">Курьер</a>-->
-<!--					<a href="#" class="link-regular">Оператор ПК</a>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--			<div class="content-view__feedback-card user__search-wrapper">-->
-<!--				<div class="feedback-card__top">-->
-<!--					<div class="user__search-icon">-->
-<!--						<a href="#"><img src="./img/user-man.jpg" width="65" height="65"></a>-->
-<!--						<span>2 заданий</span>-->
-<!--						<span>1 отзывов</span>-->
-<!--					</div>-->
-<!--					<div class="feedback-card__top--name user__search-card">-->
-<!--						<p class="link-name"><a href="#" class="link-regular">Крючков Василий</a></p>-->
-<!--						<span></span><span></span><span></span><span></span><span class="star-disabled"></span>-->
-<!--						<b>4.25</b>-->
-<!--						<p class="user__search-content">-->
-<!--							Разнообразный и богатый опыт говорит нам, что существующая теория способствует-->
-<!--							подготовке и реализации форм воздействия. Безусловно, укрепление и развитие-->
-<!--							внутренней структуры представляет собой интересный эксперимент-->
-<!--						</p>-->
-<!--					</div>-->
-<!--					<span class="new-task__time">Был на сайте минуту назад</span>-->
-<!--				</div>-->
-<!--				<div class="link-specialization user__search-link--bottom">-->
-<!--					<a href="#" class="link-regular">Ремонт</a>-->
-<!--					<a href="#" class="link-regular">Курьер</a>-->
-<!--					<a href="#" class="link-regular">Оператор ПК</a>-->
-<!--				</div>-->
-<!--			</div>-->
+
 		</section>
 
 		<section class="search-task">
