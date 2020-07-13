a288f99c3f0698cb3f64231561bcb2f379cde367
Александр Зубрицкий <5998971@gmail.com>
2020-07-13 21:21:25 +0300

    переписывает блоки условных конструкций на switch

--- frontend/models/UserInfo.php
+++ frontend/models/UserInfo.php
@@ -127,40 +127,29 @@ class UserInfo extends ActiveRecord
 		// При большем числе записей следует показывать их через пагинацию.
 		foreach ($value as $key => $item) {
 			if ($item) {
-				if ($key === 'categories') {
-					$query->joinWith('userCategories uc')->where(['uc.category_id' => $item]);
-				}
-				//«Сейчас онлайн» — добавляет к условию фильтрации показ исполнителей,
-				// время последней активности которых было не больше получаса назад
-				if ($key === 'online') {
-					$query->andWhere(['<=', 'online', date("Y-m-d H:i:s", strtotime("+3 hour"))]);
-					$query->andWhere(['>=', 'online', date("Y-m-d H:i:s", strtotime("+150 minutes"))]);
-				}
-
-				//«Сейчас свободен» — добавляет к условию фильтрации показ исполнителей,
-				// для которых сейчас нет назначенных активных заданий
-				if ($key === 'isFree') {
-					$query->joinWith('tasks t');
-					$query->andWhere(['t.executor_id' => null]);
-				}
-
-				//«Есть отзывы» — добавляет к условию фильтрации показ исполнителей с отзывами
-				if ($key === 'review') {
-					$query->joinWith('review r');
-					$query->andWhere(['not', ['r.user_id' => null]]);
-				}
-
-				//«В избранном» — добавляет к условию фильтрации показ пользователей,
-				// которые были добавлены в избранное
-				if ($key === 'favorite') {
-					$query->joinWith('favorites f');
-					$query->andWhere(['f.user_selected_id'=>null]);
-				}
-
-				//Поле «Поиск по имени» сбрасывает все выбранные фильтры и
-				// ищет пользователя с нестрогим совпадением по его имени.
-				if ($key === 'search') {
-					$query->andWhere(['LIKE', 'name', $item]);
+				switch ($key) {
+					case 'categories':
+						$query->joinWith('userCategories uc')->where(['uc.category_id' => $item]);
+						break;
+					case 'online':
+						$query->andWhere(['<=', 'online', date("Y-m-d H:i:s", strtotime("+3 hour"))]);
+						$query->andWhere(['>=', 'online', date("Y-m-d H:i:s", strtotime("+150 minutes"))]);
+						break;
+					case 'isFree':
+						$query->joinWith('tasks t');
+						$query->andWhere(['t.executor_id' => null]);
+						break;
+					case 'review':
+						$query->joinWith('review r');
+						$query->andWhere(['not', ['r.user_id' => null]]);
+						break;
+					case 'favorite':
+						$query->joinWith('favorites f');
+						$query->andWhere(['f.user_selected_id'=>null]);
+						break;
+					case 'search':
+						$query->andWhere(['LIKE', 'name', $item]);
+						break;
 				}
 			}
 
