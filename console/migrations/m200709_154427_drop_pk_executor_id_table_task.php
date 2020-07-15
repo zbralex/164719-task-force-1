f497ba5db264732aaa4530d2fba857e0af5c2858
Alexander Zubritsky <5998971@gmail.com>
2020-07-15 10:03:39 +0300

    Update console/migrations/m200709_154427_drop_pk_executor_id_table_task.php
    Co-authored-by: Anatoly Pashin <anatoly.pashin@gmail.com>

--- console/migrations/m200709_154427_drop_pk_executor_id_table_task.php
+++ console/migrations/m200709_154427_drop_pk_executor_id_table_task.php
@@ -25,19 +25,4 @@ class m200709_154427_drop_pk_executor_id_table_task extends Migration
     {
 
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m200709_154427_drop_pk_executor_id_table_task cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
