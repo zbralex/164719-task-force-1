90adeb967b606b15dd14821ef375de6fea49eab8
Александр Зубрицкий <5998971@gmail.com>
2020-07-31 09:57:45 +0300

    убирает регистрацию из signup и вьюху

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -149,23 +149,7 @@ class SiteController extends Controller
         return $this->render('test');
     }
 
-    /**
-     * Signs user up.
-     *
-     * @return mixed
-     */
-    public function actionSignup()
-    {
-        $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post()) && $model->signup()) {
-            Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
-            return $this->goHome();
-        }
 
-        return $this->render('signup', [
-            'model' => $model,
-        ]);
-    }
 
     /**
      * Requests password reset.
