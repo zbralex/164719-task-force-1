60d7f87f12c06f50339110e9b6dbb16ce85eb200
Alexander Zubritsky <5998971@gmail.com>
2020-08-11 10:06:12 +0300

    Update frontend/views/layouts/landing.php
    Co-authored-by: Anatoly Pashin <anatoly.pashin@gmail.com>

--- frontend/views/layouts/landing.php
+++ frontend/views/layouts/landing.php
@@ -2,13 +2,7 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
-use common\widgets\Alert;
-use frontend\models;
 
 AppAsset::register($this);
 ?>
