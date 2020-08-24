613e044adeb8bd9ccd593db07e42393af2e03944
Александр Зубрицкий <5998971@gmail.com>
2020-08-24 14:00:40 +0300

    дефолтное имя пользователя

--- frontend/views/tasks/view.php
+++ frontend/views/tasks/view.php
@@ -5,119 +5,142 @@
  * @var $count_tasks = ''
  * @var $user = ''
  */
+
+use frontend\models\User;
+use frontend\models\UserCategory;
 use yii\helpers\Url;
+//use yii\web\User;
+$user_data =  User::findOne(Yii::$app->user->id);
 ?>
 <main class="page-main">
-	<div class="main-container page-container">
-		<section class="content-view">
-			<div class="content-view__card">
-				<div class="content-view__card-wrapper">
-					<div class="content-view__header">
-						<div class="content-view__headline">
-							<h1><?= $detail->name?></h1>
-							<span>Размещено в категории
-                                    <a href="#" class="link-regular"><?= $detail->category->name?></a>
+    <div class="main-container page-container">
+        <section class="content-view">
+            <div class="content-view__card">
+                <div class="content-view__card-wrapper">
+                    <div class="content-view__header">
+                        <div class="content-view__headline">
+                            <h1><?= $detail->name ?></h1>
+                            <span>Размещено в категории
+                                    <a href="#" class="link-regular"><?= $detail->category->name ?></a>
                                    <?= Yii::$app->formatter->asRelativeTime(strtotime($detail->created_at)) ?></span>
-						</div>
-						<b class="new-task__price new-task__price--clean content-view-price"><?= $detail->price;?><b> ₽</b></b>
-						<div class="new-task__icon new-task__icon--clean content-view-icon"></div>
-					</div>
-					<div class="content-view__description">
-						<h3 class="content-view__h3">Общее описание</h3>
-						<p>
-							<?= $detail->description?>
-						</p>
-					</div>
-					<div class="content-view__attach">
-						<h3 class="content-view__h3">Вложения</h3>
+                        </div>
+                        <b class="new-task__price new-task__price--clean content-view-price"><?= $detail->price; ?><b>
+                                ₽</b></b>
+                        <div class="new-task__icon new-task__icon--clean content-view-icon"></div>
+                    </div>
+                    <div class="content-view__description">
+                        <h3 class="content-view__h3">Общее описание</h3>
+                        <p>
+                            <?= $detail->description ?>
+                        </p>
+                    </div>
+                    <div class="content-view__attach">
+                        <h3 class="content-view__h3">Вложения</h3>
 
-                        <?php foreach ($detail->attachments as $item):?>
+                        <?php foreach ($detail->attachments as $item): ?>
 
-                            <a href="<?= Url::to($item->url); ?>" class="link-regular"><?= $item->name;?></a><br>
+                            <a href="<?= Url::to($item->url); ?>" class="link-regular"><?= $item->name; ?></a><br>
 
                         <?php endforeach; ?>
 
-					</div>
-					<div class="content-view__location">
-						<h3 class="content-view__h3">Расположение</h3>
-						<div class="content-view__location-wrapper">
-							<div class="content-view__map">
-								<a href="#"><img src="./img/map.jpg" width="361" height="292"
-										alt="Москва, Новый арбат, 23 к. 1"></a>
-							</div>
-							<div class="content-view__address">
-								<span class="address__town">Москва</span><br>
-								<span>Новый арбат, 23 к. 1</span>
-								<p>Вход под арку, код домофона 1122</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="content-view__action-buttons">
-					<button class=" button button__big-color response-button open-modal"
-						type="button" data-for="response-form">Откликнуться</button>
-					<button class="button button__big-color refusal-button open-modal"
-						type="button" data-for="refuse-form">Отказаться</button>
-					<button class="button button__big-color request-button open-modal"
-						type="button" data-for="complete-form">Завершить</button>
-				</div>
-			</div>
-			<div class="content-view__feedback">
-				<h2>Отклики <span>(<?= count($detail->response)?>)</span></h2>
-				<div class="content-view__feedback-wrapper">
-					<div class="content-view__feedback-card">
-                        <?php foreach ($detail->response as $item):?>
-						<div class="feedback-card__top">
-							<a href="#"><img src="./img/man-blond.jpg" width="55" height="55"></a>
-							<div class="feedback-card__top--name">
-								<p class="link-name"><a href="#" class="link-regular">
-                                        <?= $item->userInfo->name;?>
-                                        <?= $item->userInfo->surname;?>
-                                    </a></p>
-								<span></span><span></span><span></span><span></span><span class="star-disabled"></span>
-								<b>4.25</b>
-							</div>
-							<span class="new-task__time">
+                    </div>
+                    <div class="content-view__location">
+                        <h3 class="content-view__h3">Расположение</h3>
+                        <div class="content-view__location-wrapper">
+                            <div class="content-view__map">
+                                <a href="#"><img src="./img/map.jpg" width="361" height="292"
+                                                 alt="Москва, Новый арбат, 23 к. 1"></a>
+                            </div>
+                            <div class="content-view__address">
+                                <span class="address__town">Москва</span><br>
+                                <span>Новый арбат, 23 к. 1</span>
+                                <p>Вход под арку, код домофона 1122</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-view__action-buttons">
+                    <button class=" button button__big-color response-button open-modal"
+                            type="button" data-for="response-form">Откликнуться
+                    </button>
+                    <button class="button button__big-color refusal-button open-modal"
+                            type="button" data-for="refuse-form">Отказаться
+                    </button>
+                    <button class="button button__big-color request-button open-modal"
+                            type="button" data-for="complete-form">Завершить
+                    </button>
+                </div>
+            </div>
+            <div class="content-view__feedback">
+                <h2>Отклики <span>(<?= count($detail->response) ?>)</span></h2>
+                <div class="content-view__feedback-wrapper">
+                    <div class="content-view__feedback-card">
+                        <?php foreach ($detail->response as $item): ?>
+                            <div class="feedback-card__top">
+                                <a href="#"><img src="./img/man-blond.jpg" width="55" height="55"></a>
+                                <div class="feedback-card__top--name">
+                                    <p class="link-name"><a href="#" class="link-regular">
+                                            <?php
+                                            if ($item->userInfo) {
+                                                echo $detail->author->name . ' ' . $detail->author->surname;
+                                            } else {
+                                               echo  $user_data->name;
+                                            }
+                                            ?>
+
+                                        </a></p>
+                                    <span></span><span></span><span></span><span></span><span
+                                        class="star-disabled"></span>
+                                    <b>4.25</b>
+                                </div>
+                                <span class="new-task__time">
                                  <?= Yii::$app->formatter->asRelativeTime(strtotime($item->responsed_at)) ?>
                             </span>
-						</div>
-						<div class="feedback-card__content">
-							<p>
-                                <?= $item->comment;?>
-							</p>
-							<span><?= $item->price;?> ₽</span>
-						</div>
-						<div class="feedback-card__actions">
-							<a class="button__small-color request-button button"
-								type="button">Подтвердить</a>
-							<a class="button__small-color refusal-button button"
-								type="button">Отказать</a>
-						</div>
+                            </div>
+                            <div class="feedback-card__content">
+                                <p>
+                                    <?= $item->comment; ?>
+                                </p>
+                                <span><?= $item->price; ?> ₽</span>
+                            </div>
+                            <div class="feedback-card__actions">
+                                <a class="button__small-color request-button button"
+                                   type="button">Подтвердить</a>
+                                <a class="button__small-color refusal-button button"
+                                   type="button">Отказать</a>
+                            </div>
                         <?php endforeach; ?>
-					</div>
-				</div>
-			</div>
-		</section>
-		<section class="connect-desk">
-			<div class="connect-desk__profile-mini">
-				<div class="profile-mini__wrapper">
-					<h3>Заказчик</h3>
-					<div class="profile-mini__top">
-						<img src="./img/man-brune.jpg" width="62" height="62" alt="Аватар заказчика">
-						<div class="profile-mini__name five-stars__rate">
-							<p><?= $detail->author->name?> <?= $detail->author->surname?></p>
-						</div>
-					</div>
-					<p class="info-customer"><span><?= $count_tasks; ?> заданий</span>
-						<span class="last-">2 года на сайте</span></p>
-					<a href="<?= Url::to(['user/view/'.$detail->author->id]); ?>" class="link-regular">Смотреть профиль</a>
-				</div>
-			</div>
-			<div id="chat-container">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="connect-desk">
+            <div class="connect-desk__profile-mini">
+                <div class="profile-mini__wrapper">
+                    <h3>Заказчик</h3>
+                    <div class="profile-mini__top">
+                        <img src="./img/man-brune.jpg" width="62" height="62" alt="Аватар заказчика">
+                        <div class="profile-mini__name five-stars__rate">
+                            <p><?php if ($detail->author) {
+                                    echo $detail->author->name . ' ' . $detail->author->surname;
+                                } else {
+                                    echo $user_data->name;
+                                } ?></p>
+                        </div>
+                    </div>
+                    <p class="info-customer"><span><?= $count_tasks; ?> заданий</span>
+                        <span class="last-">2 года на сайте</span></p>
+                    <?php if ($detail->author): ?>
+                        <a href="<?= Url::to(['user/view/' . $detail->author->id]); ?>" class="link-regular">Смотреть
+                            профиль</a>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div id="chat-container">
 
-				<!--                    добавьте сюда атрибут task с указанием в нем id текущего задания-->
-				<chat class="connect-desk__chat"></chat>
-			</div>
-		</section>
-	</div>
+                <!--                    добавьте сюда атрибут task с указанием в нем id текущего задания-->
+                <chat class="connect-desk__chat"></chat>
+            </div>
+        </section>
+    </div>
 </main>
