8e87899d67d561ded05da948ddcb1a4e6c1d4597
Александр Зубрицкий <5998971@gmail.com>
2020-08-31 11:21:34 +0300

    переносит логику фильтра в отдельный класс

--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace frontend\controllers;
 
 use frontend\models\forms\UserForm;
 use frontend\models\UserInfo;
+use taskForce\classes\utils\FilterUtil;
 use Yii;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -30,34 +31,8 @@ class UsersController extends SecuredController
 			$query = UserInfo::find()
 				->joinWith('user u')
 				->orderBy('u.created_at ASC');
-			foreach ($formContent as $key => $item) {
-				if ($item) {
-					switch ($key) {
-						case 'categories':
-							$query->joinWith('userCategories uc')->where(['uc.category_id' => $item]);
-							break;
-						case 'online':
-							$query->andWhere(['<=', 'online', date("Y-m-d H:i:s", strtotime("+3 hour"))]);
-							$query->andWhere(['>=', 'online', date("Y-m-d H:i:s", strtotime("+150 minutes"))]);
-							break;
-						case 'isFree':
-							$query->joinWith('tasks t');
-							$query->andWhere(['t.executor_id' => null]);
-							break;
-						case 'review':
-							$query->joinWith('review r');
-							$query->andWhere(['not', ['r.user_id' => null]]);
-							break;
-						case 'favorite':
-							$query->joinWith('favorites f');
-							$query->andWhere(['f.user_selected_id' => null]);
-							break;
-						case 'search':
-							$query->andWhere(['LIKE', 'user_info.name', $item]);
-							break;
-					}
-				}
-			}
+
+            FilterUtil::formFilter($formContent, $query);
 
 			$users = $query->all();
 		}
---
+++ src/classes/utils/FilterUtil.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace taskForce\classes\utils;
+
+
+abstract class FilterUtil {
+    public static function formFilter($form, $query) {
+        foreach ($form as $key => $item) {
+            if ($item) {
+                switch ($key) {
+                    case 'categories':
+                        $query->joinWith('userCategories uc')->where(['uc.category_id' => $item]);
+                        break;
+                    case 'online':
+                        $query->andWhere(['<=', 'online', date("Y-m-d H:i:s", strtotime("+3 hour"))]);
+                        $query->andWhere(['>=', 'online', date("Y-m-d H:i:s", strtotime("+150 minutes"))]);
+                        break;
+                    case 'isFree':
+                        $query->joinWith('tasks t');
+                        $query->andWhere(['t.executor_id' => null]);
+                        break;
+                    case 'review':
+                        $query->joinWith('review r');
+                        $query->andWhere(['not', ['r.user_id' => null]]);
+                        break;
+                    case 'favorite':
+                        $query->joinWith('favorites f');
+                        $query->andWhere(['f.user_selected_id' => null]);
+                        break;
+                    case 'search':
+                        $query->andWhere(['LIKE', 'user_info.name', $item]);
+                        break;
+                }
+            }
+        }
+
+    }
+}
