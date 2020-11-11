19533bacc09f353b026c103960d57c82012b0da1
Александр Зубрицкий <5998971@gmail.com>
2020-11-11 10:22:57 +0300

    move bundle asset on head

--- frontend/assets/YandexAPIKey.php
+++ frontend/assets/YandexAPIKey.php
@@ -8,4 +8,5 @@ class YandexAPIKey extends AssetBundle {
 	public $js = [
 		'https://api-maps.yandex.ru/2.1/?apikey=e666f398-c983-4bde-8f14-e3fec900592a&lang=ru_RU',
 	];
+	public $jsOptions = ['position' => \yii\web\View::POS_HEAD];
 }
