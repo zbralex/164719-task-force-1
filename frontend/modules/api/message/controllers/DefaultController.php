2be92e1e753ba1ab291b2f82f86b20a7d9249bb3
Александр Зубрицкий <5998971@gmail.com>
2020-11-20 17:19:34 +0300

    добавляет crud

--- frontend/modules/api/message/controllers/DefaultController.php
+++ frontend/modules/api/message/controllers/DefaultController.php
@@ -2,19 +2,32 @@
 
 namespace app\modules\api\message\controllers;
 
+use frontend\models\Message;
+use yii\data\ActiveDataProvider;
+use yii\rest\ActiveController;
 use yii\web\Controller;
 
 /**
  * Default controller for the `message` module
  */
-class DefaultController extends Controller
+class DefaultController extends ActiveController
 {
+	public $modelClass = 'frontend\models\Message';
     /**
      * Renders the index view for the module
      * @return string
      */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+	public function actionIndex()
+	{
+
+		$dataProvider = new ActiveDataProvider([
+			'query' => Message::find(),
+		]);
+
+		return $this->render('index', [
+			'dataProvider' => $dataProvider,
+		]);
+	}
+
+
 }
