671c1ccd5fe0703cde0e960e85b0f31d9b6a9a8c
Александр Зубрицкий <5998971@gmail.com>
2020-11-28 20:39:56 +0300

    удаляет из модели тестовую функцию

--- frontend/models/Message.php
+++ frontend/models/Message.php
@@ -74,8 +74,4 @@ class Message extends ActiveRecord
     {
         return $this->hasOne(Task::className(), ['id' => 'task_id']);
     }
-	public function getMessage()
-	{
-		return $this->hasMany(Task::className(), ['id' => 'task_id']);
-	}
 }
