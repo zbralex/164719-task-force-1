5395bdaa6f9c887b73525870fa7939c4c2e62fcc
Александр Зубрицкий <5998971@gmail.com>
2020-12-27 17:22:56 +0300

    восстановление БД

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=TASK_FORCE',
             'username' => 'root',
-            'password' => '',
+            'password' => '!MyNewPass1',
             'charset' => 'utf8',
         ],
         'mailer' => [
