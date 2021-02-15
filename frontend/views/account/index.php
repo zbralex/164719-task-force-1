1b25f0a8abd725dad7c8347571669e04999e8bd4
Александр Зубрицкий <5998971@google.com>
2021-02-15 19:30:13 +0300

    добавляет JUI для даты

--- frontend/views/account/index.php
+++ frontend/views/account/index.php
@@ -44,18 +44,46 @@ $formatter = \Yii::$app->formatter;
                                 <input class="input textarea" id="200" name="" placeholder="<?= $userInfo->user->name?>" disabled value="<?= $userInfo->user->name?>">
                             </div>
                             <div class="field-container account__input account__input--email">
-                                <label for="201">email</label>
-                                <input class="input textarea" id="201" name="" placeholder="DenisT@bk.ru" value="<?= $userInfo->user->email?>">
+
+
+                                <?= $form->field($model, 'email', [
+                                    'options' => ['tag' => false]
+                                ])->textInput([
+                                    'class' => 'input textarea',
+                                    'placeholder' => 'DenisT@bk.ru',
+                                    'value' => $userInfo->user->email
+                                ]) ?>
                             </div>
                             <div class="field-container account__input account__input--address">
-                                <label for="202">Адрес</label>
-                                <input class="input textarea" id="202" name=""
-                                       placeholder="Санкт-Петербург, Московский район" value="<?= $userInfo->city->city?>">
+
+                                <?= $form->field($model, 'address', [
+                                    'options' => ['tag' => false]
+                                ])->textInput([
+                                    'class' => 'input textarea',
+                                    'placeholder' => 'Санкт-Петербург, Московский район',
+                                    'value' => $userInfo->city->city
+                                ]) ?>
+
+
                             </div>
                             <div class="field-container account__input account__input--date">
-                                <label for="203">День рождения</label>
-                                <input id="203" class="input-middle input input-date" type="text"
-                                       placeholder="15.08.1987" value="<?= $formatter->asDate($userInfo->date_birth, 'php:d.m.Y')?>">
+
+                                <?= $form->field($model, 'date_of_birth', [
+                                    'options' => ['tag' => false]
+                                ])->widget(\yii\jui\DatePicker::className(),
+                                    [ 'dateFormat' => 'php:m/d/Y',
+                                        'clientOptions' => [
+                                            'changeYear' => true,
+                                            'changeMonth' => true,
+                                            'yearRange' => '-50:-12',
+                                            'altFormat' => 'yy-mm-dd',
+
+                                        ]],['placeholder' => 'dd.mm.yyyy'])
+                                    ->textInput([
+                                        'placeholder' => \Yii::t('app', '15.08.1987'),
+                                        'class'=> 'input-middle input input-date',
+                                        'value' => $formatter->asDate($userInfo->date_birth, 'php:d.m.Y'),
+                                    ]) ;?>
                             </div>
                             <div class="field-container account__input account__input--info">
                                 <?= $form->field($model, 'about_myself', [
@@ -112,25 +140,23 @@ $formatter = \Yii::$app->formatter;
                     <h3 class="div-line">Безопасность</h3>
                     <div class="account__redaction-section-wrapper account__redaction">
                         <div class="field-container account__input">
-<!--                            <label for="211">Новый пароль</label>-->
-<!--                            <input class="input textarea" type="password" id="211" name="" value="moiparol">-->
+
                             <?= $form->field($model, 'password', [
                                 'options' => ['tag' => false]
                             ])->passwordInput([
                                 'class' => 'input textarea',
 
                             ]) ?>
+
                         </div>
                         <div class="field-container account__input">
-<!--                            <label for="212">Повтор пароля</label>-->
-<!--                            <input class="input textarea" type="password" id="212" name="" value="moiparol">-->
 
                             <?= $form->field($model, 're_password', [
                                 'options' => ['tag' => false]
                             ])->passwordInput([
                                 'class' => 'input textarea',
-
                             ]) ?>
+
                         </div>
                     </div>
 
