f0ebe3064f20f35286f4fc39b8a68ac21c4611c7
Александр Зубрицкий <5998971@google.com>
2021-03-07 23:34:58 +0300

    добавляет загрузку фото работ

--- frontend/controllers/AccountController.php
+++ frontend/controllers/AccountController.php
@@ -25,13 +25,25 @@ class AccountController extends \yii\web\Controller
 
     public function actionUploadFile() {
             $fileName = 'file';
-            $uploadPath =  Yii::getAlias('@app') . '/web/upload/';
+
 
             $files = UploadedFile::getInstancesByName($fileName);
-            var_dump($files);
-            //echo  Json::encode($_FILES);
+            $dir = Yii::getAlias('@app') . '/web/upload/' . date("Y-m-d") .'_'. date("H-m") . '/';
+
+            if(!is_dir($dir)) {
+                mkdir($dir, 0777);
+            }
             foreach($files as $file) {
-                $file->saveAs( $uploadPath . $file->baseName . '.' . $file->extension);
+
+                $file->saveAs( $dir . $file->baseName . '.' . $file->extension);
+
+                $portfolioPhoto = new PortfolioPhoto();
+                $portfolioPhoto->user_id = Yii::$app->user->identity->getId();
+                $portfolioPhoto->rating = 0;
+                $portfolioPhoto->url = '/upload/' . date("Y-m-d") .'_'. date("H-m") . '/' . $file->baseName . '.' . $file->extension;
+                $portfolioPhoto->title = $file->baseName;
+                $portfolioPhoto->description = $file->baseName;
+                $portfolioPhoto->save(false);
             }
 
     }
@@ -78,10 +90,6 @@ class AccountController extends \yii\web\Controller
                     $path = '/upload/' . date("Y-m-d") .'_'. date("H-m") . '/' . $model->userPic->baseName . '.' . $model->userPic->extension;
                     $userInfo->user_pic = $path;
                 }
-
-
-
-
 
                 $userInfo->save(false);
 
