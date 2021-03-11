438525c6c5e0b7c0dbba2e8d5fad2bedc5b227e8
Александр Зубрицкий <5998971@google.com>
2021-03-11 12:30:11 +0300

    помечает уведомленыя просмотренными

--- frontend/controllers/EventsController.php
+++ frontend/controllers/EventsController.php
@@ -6,7 +6,10 @@ use \yii\web\Controller;
 
 class EventsController extends Controller {
     public function actionIndex() {
-        $notifications = Notification::find()->where(['user_id'=> Yii::$app->user->identity->getId()])->all();
+        $notifications = Notification::find()
+            ->where(['user_id'=> Yii::$app->user->identity->getId()])
+            ->andWhere(['and', ['is_view' => 0]])
+            ->all();
 
         foreach ($notifications as $notification) {
             $notification->is_view = 1;
